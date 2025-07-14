f8d708262c7202b99718791cd27cb74d5b1b4c28
watabe17 <watabe.h.work@gmail.com>
2025-07-14 14:42:12 +0900

    fix: authorize function for credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -15,22 +15,16 @@ export const authOptions: NextAuthOptions = {
         if (!credentials?.email || !credentials?.password) {
           return null;
         }
-
         const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
+          where: { email: credentials?.email },
         });
 
-        if (!user) return null;
+        if (!user || !user.password) return null;
 
         const isValid = await compare(credentials.password, user.password);
         if (!isValid) return null;
 
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          role: user.role,
-        };
+        return { id: user.id, name: user.name, email: user.email, role: user.role };
       },
     }),
   ],
